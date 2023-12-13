dcb18ef63362e34c81a403d0e24d2544f816b121
fedusiv <fedusivilya@gmail.com>
2023-12-13 18:25:57 +0200

    Now status line is also updated when edtior states has changed. Display different information

--- source/gui/gui_configs.hpp
+++ source/gui/gui_configs.hpp
@@ -2,7 +2,11 @@
 #define __GUI_CONFIGS_HPP__
 #include "vec2.hpp"
 
-
 #define WIDGET_EDITOR_BUFFER_NAME_WRITE_OFFSET Vec2(3,0)
 #define WIDGET_BORDER_THICK 0
+
+namespace GuiConstants {
+    const std::string CMD_STRING = "Cmd";
+}
+
 #endif // __GUI_CONFIGS_HPP__
