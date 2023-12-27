263cc2005652f330c301063e4a1bc5a87dbf39de
Ilya Fedusiv <ilya.fedusiv@servotronix.com>
2023-12-27 11:41:54 +0200

    Made successful compilation for Windows OS. 1. Improved CMake files to run on Windows. 2. Created .bat file to build on windows. 3. Small fixed in the code.

--- source/common/macros.h
+++ source/common/macros.h
@@ -6,7 +6,7 @@
 #define UNUSED(expr) do { (void)(expr); } while (0)
 
 void printLineInfo(const char* file, const char* function, int line);
-#define PRINT_LINE_INFO() printInfo(__FILE__, __FUNCTION__, __LINE__)
+#define PRINT_LINE_INFO() printLineInfo(__FILE__, __FUNCTION__, __LINE__)
 
 // This macro function allows to print custom class.
 // Requirement for class is need to have function getPrint() which returns std::vector or other interatable container of strings
